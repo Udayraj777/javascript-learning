4e907a0b0f9eb2a2ab221529f1b078abcd9cb818
Udayraj777 <udayrajmishra007@gmail.com>
2025-11-21 08:37:31 +0530

    data type

---
+++ day-2/data_types.js
@@ -0,0 +1,30 @@
+// data types:
+
+// 1.String
+let FirstName="Uday"
+
+// 2.number
+let age=24
+
+// 3.boolean
+let GoingToSuccessfull=true
+
+// 4.then there is object
+let user={
+    "name":"Uday",
+    "age":"24"
+}
+
+// 5. there is null value in javascript which has object type
+
+let temp=null
+
+// 6. we check type of every variable with the help of typeof
+console.log(typeof FirstName)
+console.log(typeof age)
+console.log(typeof user)
+console.log(typeof GoingToSuccessfull)
+
+// 7.there is undefined for variable which has no assignment means they are empty
+let value=undefined;
+console.log(typeof value)
