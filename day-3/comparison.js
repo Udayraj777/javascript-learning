e1ff071a5193304b9bcfe456f79394829b491109
Udayraj777 <udayrajmishra007@gmail.com>
2025-11-22 13:12:51 +0530

    comparison of datatypes and exceptions

---
+++ day-3/comparison.js
@@ -0,0 +1,17 @@
+// console.log("5"<6);
+
+// console.log(5<"6");
+
+// console.log("5"==5)
+
+// console.log(null<0);
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+
+
+
+console.log(undefined<0);
+console.log(undefined>0);
+console.log(undefined==0);
+console.log(undefined>=0);
