c0dcb7e681e08e48716b292a407f76643dfa5ddd
Udayraj777 <udayrajmishra007@gmail.com>
2025-11-23 10:52:21 +0530

    datatype_summary

---
+++ day-4/datatype_summary.js
@@ -0,0 +1,17 @@
+// we already know other datatype only symbol and big int left 
+
+// symbol
+
+let a = Symbol('123');
+let b=Symbol('123');
+
+console.log(a==b);
+
+
+console.log(typeof a);
+
+// big int
+
+const bign = 23764896324898764n
+
+console.log(typeof bign);
