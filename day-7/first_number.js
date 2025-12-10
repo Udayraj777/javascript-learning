f3b8337648596de656a59e93b22173b81b6292a3
Udayraj777 <udayrajmishra007@gmail.com>
2025-12-10 17:25:31 +0530

    learn number and math in javascript

---
+++ day-7/first_number.js
@@ -0,0 +1,14 @@
+const score= 400
+console.log(score)
+const balance = new Number(100.1234)
+
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber=123.8996
+
+console.log(otherNumber.toPrecision(5))
+const hundreds=1000000;
+console.log(hundreds.toLocaleString('en-US'))
---
+++ day-7/second_maths.js
@@ -0,0 +1,24 @@
+console.log(Math.abs(-44))
+// this convert negative to positive
+console.log(Math.round(48.7))
+// this roundof the value similarly like us 
+
+console.log(Math.ceil(4.3))
+// give the ceil value
+// console.log(Math.floor(4.9))
+
+// console.log(Math.sqrt(36))
+
+// console.log(Math.pow(5,2))
+
+// console.log(Math.min(3,5,8,9))
+
+// console.log(Math.max(3,5,8,9))
+
+
+// console.log(Math.floor((Math.random()*10)+1))
+
+const min =1 
+const max=200
+
+console.log(Math.floor((Math.random())*(max-min +1))+min)
