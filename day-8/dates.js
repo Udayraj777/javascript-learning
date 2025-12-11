594032df97feef8596420816ce0305309eee4180
Udayraj777 <udayrajmishra007@gmail.com>
2025-12-11 16:27:11 +0530

    date learn

---
+++ day-8/dates.js
@@ -0,0 +1,16 @@
+let myDate = new Date()
+
+console.log(myDate.toString());
+
+console.log(myDate.toDateString());
+
+console.log(myDate.toLocaleString());
+
+console.log(typeof myDate)
+
+
+// create a date/
+let CreatedDate = new Date(2023,0,23,5,3)
+
+let myCreatedDate=new Date("2023-01-04")
+console.log(myCreatedDate.toLocaleString());
